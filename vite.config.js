2a822f873983a211881b62044b8896057b3b977a
N-meapp <developersnme@gmail.com>
2025-07-03 14:46:53 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["three", "three/examples/jsm/loaders/GLTFLoader"],
+  // optimizeDeps: {
+  //   include: ["three", "three/examples/jsm/loaders/GLTFLoader"],
+  // },
+  build: {
+    sourcemap: false, // ⛔ Turn off source maps
   },
 });
